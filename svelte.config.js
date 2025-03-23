927d61c5866b9ac3f90cb8a4f607e61ae1d520dd
lubobrowski <lubobrowski@gmail.com>
2025-03-23 07:22:25 +0100

    Add pages and assets set to docs

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,11 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
+			pages: 'docs',
+			assets: 'docs',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH + '/svelte-invoicing'
