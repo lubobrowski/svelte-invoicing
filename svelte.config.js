df50fcdc4992ea51319f87db98c19fee0665e5d3
lubobrowski <lubobrowski@gmail.com>
2025-03-23 07:18:14 +0100

    Changed config.kit settings for github pages hosting

--- svelte.config.js
+++ svelte.config.js
@@ -3,19 +3,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: 'index.html'
+			fallback: '404.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : ''
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH + '/svelte-invoicing'
 		}
 	}
 };
